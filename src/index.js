2ca8aa628bd64f41a6adf95a051762f341910fa7
Matheus dos Santos Pires Ferreira <mpferreira@ativainvestimentos.com.br>
2021-10-03 12:16:28 -0300

    Trabalhando com middlewares

--- src/index.js
+++ src/index.js
@@ -10,19 +10,67 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+
+  const { username } = request.headers;
+
+  const user = users.find((account) => account.username === username);
+
+  if (!user)
+    return response.status(404).json({ "error": "Account not found!" });
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+
+  const { user } = request;
+
+  if (user.pro === false && user.todos.length >= 10)
+    return response.status(403).json({ error: "Not permission to add todo" });
+
+  return next();
+
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find((account) => account.username === username);
+
+  if (!user)
+    return response.status(404).json({ "error": "Account not found!" });
+
+  const todo = user.todos.find((todo) => todo.id.toString() === id.toString());
+
+  if (!validate(id.toString()))
+    return response.status(400).json({ error: "Not valid id!" });
+
+  if (!todo)
+    return response.status(404).json({ "error": "Todo not found!" });
+
+  request.user = user;
+  request.todo = todo;
+
+  return next();
+
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+
+  const { id } = request.params;
+
+  const user = users.find((user) => user.id.toString() === id.toString())
+
+  if (!user)
+    return response.status(404).json({ "error": "Account not found!" });
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
